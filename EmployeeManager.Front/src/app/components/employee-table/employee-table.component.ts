63e41fcba16e06ee9a42acb90b10536d2ad467de
Ricardo <ricardogiani@gmail.com>
2025-07-28 23:00:15 -0300

    ajustes Front

--- EmployeeManager.Front/src/app/components/employee-table/employee-table.component.ts
+++ EmployeeManager.Front/src/app/components/employee-table/employee-table.component.ts
@@ -1,13 +1,15 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 import { Employee } from '../../interfaces/employee';
 import { JobLevelEnum } from '../../enums/job-level-enum';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-employee-table',
   standalone: true,
-  imports: [CommonModule, MatTableModule], 
+  imports: [CommonModule, MatTableModule, MatButtonModule, MatIconModule], 
   templateUrl: './employee-table.component.html',
   styleUrls: ['./employee-table.component.css'] 
 })
@@ -18,20 +20,39 @@ export class EmployeeTableComponent implements OnInit {
 
   JobLevelEnum = JobLevelEnum;
 
+  // DataSource para a mat-table
+  dataSource = new MatTableDataSource<Employee>();
+
+  displayedColumns: string[] = [
+    'id',
+    'firstName',
+    'lastName',
+    'email',
+    'documentNumber',
+    'birthDate',
+    'jobLevel',
+    'phoneNumber',
+    'active',
+    'managerId',
+    'createdAt',
+    'updatedAt',
+    'actions'
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
-    
-  }
-  
-  formatDate(date: Date | string | undefined): string {
-    if (!date) return 'N/A';
-    // Se for uma string (vindo da API), pode precisar converter para Date primeiro
-    const d = typeof date === 'string' ? new Date(date) : date;
-    // Formato básico DD/MM/AAAA
-    return d.toLocaleDateString('pt-BR');
+     if (this.employees) {
+      this.dataSource.data = this.employees;
+    }
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['employees'] && this.employees) {
+      this.dataSource.data = this.employees;
+    }
+  }
+  
   editEmployee(employee: Employee) : void
   {
     console.log(`editEmployee: ${employee}`);
